bfe89987105845024766d5bbca5f43729231fa26
KashewA <agnihotri.kaashvi@gmail.com>
2023-12-05 11:51:42 -0800

    Initial Commit

---
+++ RecipesFinal/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  RecipesFinal
+//
+//  Created by Kaashvi Agnihotri on 12/5/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ RecipesFinal/RecipesFinalApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecipesFinalApp.swift
+//  RecipesFinal
+//
+//  Created by Kaashvi Agnihotri on 12/5/23.
+//
+
+import SwiftUI
+
+@main
+struct RecipesFinalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
